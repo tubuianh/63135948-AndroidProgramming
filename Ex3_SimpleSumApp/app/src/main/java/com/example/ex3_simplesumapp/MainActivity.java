7f1839924ce0d7ddb306f00df409378394662b1f
tubuianh <anhtu2122002@gmail.com>
2024-03-06 14:54:44 +0700

    Ex3_SimpleSumApp

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -19,18 +19,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        setview();
 
 
     }
+    public void setview (){
+    inputa = findViewById(R.id.inputa);
+    inputb = findViewById(R.id.inputb);
+    outputkq = findViewById(R.id.outputkq);
+}
     public void TinhTong(View view){
-        inputa = findViewById(R.id.inputa);
-        inputb = findViewById(R.id.inputb);
-        outputkq = findViewById(R.id.outputkq);
-
         int a = Integer.parseInt(inputa.getText().toString());
         int b = Integer.parseInt(inputb.getText().toString());
-
         outputkq.setText(String.valueOf(a+b));
+    }
+    public void TinhTru(View view){
+        int a = Integer.parseInt(inputa.getText().toString());
+        int b = Integer.parseInt(inputb.getText().toString());
+        outputkq.setText(String.valueOf(a-b));
+    }
+    public void TinhNhan(View view){
+        int a = Integer.parseInt(inputa.getText().toString());
+        int b = Integer.parseInt(inputb.getText().toString());
+        outputkq.setText(String.valueOf(a*b));
+    }
+    public void TinhChia(View view){
+        float a = Float.parseFloat(inputa.getText().toString());
+        float b = Float.parseFloat(inputb.getText().toString());
+        if(b!=0)
+            outputkq.setText(String.valueOf(a/b));
+        else
+            outputkq.setText("Không chia được vì b = 0!");
     }
 }
