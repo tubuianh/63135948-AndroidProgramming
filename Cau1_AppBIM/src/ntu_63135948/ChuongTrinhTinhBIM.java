d08ae16ee624b9af42b3cc3cbef28dd4f87c2327
tubuianh <anhtu2122002@gmail.com>
2024-03-23 16:16:43 +0700

    Update

---
+++ Cau1_AppBIM/src/ntu_63135948/ChuongTrinhTinhBIM.java
@@ -0,0 +1,10 @@
+package ntu_63135948;
+
+public class ChuongTrinhTinhBIM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ManHinhChiSoBIM().setVisible(true);
+	}
+
+}
---
+++ Cau1_AppBIM/src/ntu_63135948/ManHinhChiSoBIM.java
@@ -0,0 +1,58 @@
+package ntu_63135948;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.text.DecimalFormat;
+import javax.swing.border.BevelBorder;
+public class ManHinhChiSoBIM extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtChieuCao;
+
+
+	public ManHinhChiSoBIM() {
+		
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 399, 526);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(154, 205, 50));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Chỉ Số Cơ Thể");
+		lblNewLabel.setBounds(126, 11, 137, 25);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setForeground(new Color(0, 128, 0));
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblChiuCao = new JLabel("Chiều cao(cm)");
+		lblChiuCao.setBounds(10, 99, 152, 25);
+		lblChiuCao.setForeground(new Color(0, 128, 0));
+		lblChiuCao.setFont(new Font("Tahoma", Font.BOLD, 20));
+		contentPane.add(lblChiuCao);
+
+		txtChieuCao = new JTextField();
+		txtChieuCao.setBounds(164, 99, 207, 26);
+		contentPane.add(txtChieuCao);
+		txtChieuCao.setColumns(10);
+
+
+		
+		
+		
+	}
+	
+}
